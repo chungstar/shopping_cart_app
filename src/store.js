db0ce4887d254195be741e335b0ca159f396351d
정현규 <gusrb0511@gmail.com>
2023-01-30 20:57:14 +0900

    setting commit

---
+++ src/store.js
@@ -0,0 +1,4 @@
+import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
